d5b798355feb3d80ce01cb5a938ac84e6947b9e4
Iago Effting <iago@neurocoder.dev>
2022-06-17 10:33:07 -0300

    Create for and timer

--- 9.mocking/countdown.go
+++ 9.mocking/countdown.go
@@ -1,14 +1,23 @@
 package mocking
 
 import (
+	"time"
 	"io"
 	"fmt"
 )
+
+const finalWord = "Go!"
+const countdownStart = 3
 
 // - In main we will send to os.Stdout so our users see the countdown printed to the terminal.
 // - In test we will send to bytes.Buffer so our tests can capture what data is being generated.
 
 // out *bytes.Buffer works but, it would be better to use a general purpose interface instead
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second) // not tested yet
+	}
+
+	fmt.Fprint(out, finalWord)
 }
